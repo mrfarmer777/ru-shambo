321116759a306f3ce41e12d5596cc26912917e3b
Matt Farmer <mfarmer@d120.org>
2018-07-22 19:15:54 +0000

    adds welcome#home route/view and omniauth initializer"

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  resources :users
+  
+  root "welcome#home"
+  
+  resources :users #generated via Rails g
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
