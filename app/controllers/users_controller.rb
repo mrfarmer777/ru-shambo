b0e7460a23113d5beed255c6dbdbb02f29c1705a
Matt Farmer <mfarmer@d120.org>
2018-07-23 18:52:24 +0000

    added game list to user show screen

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
         if session[:user_id]==params[:id].to_i
             #if so, move along
             @user=User.find(params[:id].to_i)
+            @games=@user.games
         else
             #if not, back to root which will send you to your own show page
             redirect_to root_path
