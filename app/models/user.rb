724d10d02c23d7fce08c9fa7ffa45fabe05aff6b
Matt Farmer <mfarmer@d120.org>
2018-07-23 14:56:56 +0000

    added user validations, not interfering with oauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ApplicationRecord
     has_many :matches, foreign_key: "challenger_id"
     has_many :opponents, through: :matches
+    validates :name, presence: true
+    validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create
+    validates :email, uniqueness: true
+    validates :uid, presence: true
+    validates :uid, numericality: {only_integer: true}
+    validates :uid, uniqueness: true
+    
 end
