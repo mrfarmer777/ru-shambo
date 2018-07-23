71a36cbb82e9125d948f87797cf8509ce0f9331b
Matt Farmer <mfarmer@d120.org>
2018-07-23 05:12:07 +0000

    implementing new match view and route

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,4 +1,29 @@
 class MatchesController < ApplicationController
+    def show
+        @match=Match.find(params[:id])
+        @challenger=@match.challenger
+        @opponent=@match.opponent
+    end
     
+    def new
+        @challenger=User.find(session[:user_id])
+        @match=Match.new
+    end
+    
+    def create
+        @match=Match.new(match_params)
+        @match.save
+        redirect_to match_path(@match)
+    end
+        
+        
+        
+    end
+    
+    private
+    
+    def match_params
+        params.require(:match).permit(:challenger_id, :opponent_id)
+    end
     
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root "welcome#home"
   
   resources :users, only: [:show, :edit, :update, :index, :destroy] #generated via Rails g
+  resources :matches, only: [:new, :create, :show, :index, :destroy]
   
   get '/auth/google_oauth2/callback' => 'sessions#create'
   
