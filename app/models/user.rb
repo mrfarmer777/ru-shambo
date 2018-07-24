4368ef4ce7861a8ef582f858f59398eeca4c97e5
Matt Farmer <mfarmer@d120.org>
2018-07-24 06:41:15 +0000

    adding user helper methods for statistics

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,35 @@ class User < ApplicationRecord
     validates :uid, numericality: {only_integer: true}
     validates :uid, uniqueness: true
     
+    
+    def wins
+        self.games.count{|g| g.result.include?("Congratulations")}
+    end
+    
+    def losses
+        self.games.count{|g| g.result.include?("Sorry")}
+    end
+    
+    def draws
+        self.games.count{|g| g.result.include?("Draw")}
+    end
+    
+    def record
+        "#{self.wins}-#{self.losses}-#{self.draws}"
+    end
+    
+    def win_percentage
+        self.wins/(self.games.count)
+    end
+    
+    def favorite_throw
+        throws=["r","p","s"]
+        throw_arr=throws.collect{|t| self.games.count{|g| g.chal_throw==t}}
+        throws[throw_arr.each_with_index.max[1]]
+    end
+    
+    def favorite_opponent
+        match=self.matches.max_by{|m| m.game_count}
+        match.opponent_name
+    end
 end
