604f8e83f171cd64b93d22afb29881c2aa2c7f5f
Matt Farmer <mfarmer@d120.org>
2018-07-26 00:31:14 +0000

    fixed bug in nested user show games route

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   resources :users, only: [:new, :create, :edit, :update, :show, :index, :destroy] do
     resources :matches, only: [:create, :show, :index, :destroy]
-    resources :games, only: [:create,:show]
+    resources :games, only: [:create,:show, :update]
   end
   
   resources :matches, only: [:show, :destroy]
