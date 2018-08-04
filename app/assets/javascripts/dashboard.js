ece59b504af2095e9433a9f102a5cedcba575f36
Matt Farmer <mfarmer@d120.org>
2018-08-04 19:21:22 +0000

    implements rendering match data to the wip panel

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -1,9 +1,9 @@
 $(function(){
-    const userData=$.get(window.location.href+"/data");
+    userData=$.get(window.location.href+"/data");
     
     userData.done(function(resp){
         console.log(resp);
-        
+        user=resp;
         const matches=resp.matches;
         const matchObjs=matches.map(function(match){
             let newMatch=new Match(match.id, match.opponent, match.created_at);
@@ -33,7 +33,7 @@ function buildMatchTable(matchObjects){
         let matchRow= $(`<tr data-id="${match.id}"></tr>`).html(`<td>${match.opp.name}</td><td>${match.startDate}</td>`);
         matchRow.addClass("match-row");
         matchRow.on("click",getMatchData);
-        matchRow.data("id",`${match.id}`);
+        //matchRow.data("id",`${match.id}`);
         $("#matches-table").append(matchRow);
     });
 }
@@ -42,8 +42,19 @@ function buildMatchTable(matchObjects){
 function getMatchData(e){
     e.preventDefault();
     const matchId=this.dataset.id;
-    $.get("/matches/"+matchId).done(function(resp){
-        console.log(resp);
-    });
+    $.get("/matches/"+matchId).done(showMatch);
     
+}
+
+function showMatch(match){
+    let oppName=match.opponent.name;
+    let oppImage=match.opponent.image;
+    let userName=user.name;
+    let userImage=user.image
+    let gameCount=match.games.length;
+    let main=$("<div></div>").html(`${userName} vs. ${oppName}`);
+    let stats=$("<div></div>").html(`Games: ${gameCount}`);
+    $("#wip").html("");
+    $("#wip").append(main).append(stats);
+  
 }
