d53faa07da9d2be4c3e06e01a6a7ae3ae70540ff
Matt Farmer <mfarmer@d120.org>
2018-08-04 17:31:36 +0000

    creating Match js object and prototype method

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -1,9 +1,26 @@
 $(function(){
-    const userData=$.get("/users/"+@user.id+"/data");
+    const userData=$.get(window.location.href+"/data");
     
     userData.done(function(resp){
         console.log(resp);
+        
+        const matches=resp.matches;
+        matches.forEach(function(match){
+            let newMatch=new Match(match.id, match.opponent, match.created_at);
+            console.log("Match Report: "+ newMatch.report());
+        });
     });
     
+});
+
+function Match(id,opp,startDate){
+    this.id=id;
+    this.opp=opp;
+    this.startDate=startDate;
 }
+
+Match.prototype.report=function(){
+    let report=`You started playing against ${this.opp.name} on ${this.startDate}.`;
+    return report;
+};
     
