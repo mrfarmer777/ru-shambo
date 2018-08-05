c52420363b9ce2895155dd658b52ce2373249068
Matt Farmer <mfarmer@d120.org>
2018-08-05 03:58:00 +0000

    pruning serializers for specific tasks

---
+++ app/serializers/user_games_serializer.rb
@@ -0,0 +1,4 @@
+class UserGamesSerializer < ActiveModel::Serializer
+  attributes :id, :opponent_name, :status
+  belongs_to :challenger, foreign_key: :challenger_id, class_name: User
+end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,6 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :name, :image, :created_at, :win_percentage, :record
-  has_many :matches
-  
-  
+
+  has_many :games, through: :matches
 end
