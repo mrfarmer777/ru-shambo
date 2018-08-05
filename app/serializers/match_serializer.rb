360eac7da0ce7070caffb4d08adf79d8af60491b
Matt Farmer <mfarmer@d120.org>
2018-08-05 15:50:38 +0000

    improving show game template and cleaning up match serialization

--- app/serializers/match_serializer.rb
+++ app/serializers/match_serializer.rb
@@ -2,5 +2,9 @@ class MatchSerializer < ActiveModel::Serializer
   attributes :id, :created_at, :opponent_name
   belongs_to :challenger, foreign_key: "challenger_id", class_name: "User" , serializer: MatchUserSerializer
   belongs_to :opponent, foreign_key: "opponent_id", class_name: "User", serializer: MatchUserSerializer
+  
   has_many :games
+  class GameSerializer < ActiveModel::Serializer
+    attributes :id, :status, :result
+  end
 end
