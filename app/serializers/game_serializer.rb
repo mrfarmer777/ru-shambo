41cc8b1362391cac05d775bc527bd588e9951180
Matt Farmer <mfarmer@d120.org>
2018-08-05 18:27:25 +0000

    updates handlebars template for show game view

--- app/serializers/game_serializer.rb
+++ app/serializers/game_serializer.rb
@@ -1,4 +1,7 @@
 class GameSerializer < ActiveModel::Serializer
   attributes :id, :chal_throw, :created_at, :status
   belongs_to :match
+  class MatchSerializer < ActiveModel::Serializer
+    attributes :challenger, :opponent
+  end
 end
